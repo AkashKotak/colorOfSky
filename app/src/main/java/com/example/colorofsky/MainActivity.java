e4a124841d02fff0868b3bfc645a38d2c8d46e73
akashkotak <akashkotak19@gmail.com>
2023-04-28 20:31:56 -0500

    Initial Commit for MSD Spring2023 HomeWork4

--- app/src/main/java/com/example/colorofsky/MainActivity.java
+++ app/src/main/java/com/example/colorofsky/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import com.airbnb.lottie.LottieAnimationView;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private LottieAnimationView lottieAnimationView;
@@ -42,12 +43,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lottieAnimationView.setAnimation("sunrise_1.json"); // Replace with the name of your sunrise animation file
         lottieAnimationView.setRepeatCount(0);
         lottieAnimationView.playAnimation();
+        MediaPlayer sunriseSound = MediaPlayer.create(this, R.raw.sunrise_sound); // Replace 'sunrise_sound' with the name of your sunrise sound file
+        sunriseSound.start();
     }
 
     private void animateSunset() {
         lottieAnimationView.setAnimation("sunset.json"); // Replace with the name of your sunset animation file
         lottieAnimationView.setRepeatCount(0);
         lottieAnimationView.playAnimation();
+        MediaPlayer sunsetSound = MediaPlayer.create(this, R.raw.sunset_sound); // Replace 'sunset_sound' with the name of your sunset sound file
+        sunsetSound.start();
     }
 
 }
